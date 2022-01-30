160afce514bff6f016ac4b8f137625755a59325e
Sparkle-Shadlyn <85486310+Sparkle-Shadlyn@users.noreply.github.com>
2022-01-30 17:35:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-function startClassification() {
-    navigator.mediaDevices.getUserMedia({audio: true});
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/27nWZp3PJ/model.json', modelReady);
+menu_list_array = ["Veg Margherita Pizza","Olive pizza", "Capsicum delight pizza"
+                    ];
+
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_addedmenu").innerHTML = htmldata;
 }
-function modelReady(){
-    classifier.classify(gotResults);
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
 }
